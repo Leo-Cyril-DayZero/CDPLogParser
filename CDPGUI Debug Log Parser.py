dbeb7f4c921c4b83b188a4f3f51d3dd0c217b7d3
CLeoDayZero <cyril.leo@dayzerodiagnostics.com>
2023-01-12 10:55:19 -0500

    collecting unmatched log lines

--- CDPGUI Debug Log Parser.py
+++ CDPGUI Debug Log Parser.py
@@ -8,11 +8,17 @@ log_file = "2022-11-30_100341_Debug.log"
 # The output json file
 json_file = "output.json"
 
+# The output file for unmatched entries
+unmatched_file = "unmatched.log"
+
 # A list to hold the parsed log entries
 log_entries = []
 
+# A list to hold the log entries that don't match any of the patterns
+unmatched_entries = []
+
 # Define the patterns for the different log formats
-pattern1 = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) \[(\d+)\] \[(\w+)\] (\w+[\w\s]+) ([\w\/]+): (\w+[\w\s]+): ([\w-]+)"
+pattern1 = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) \[(\d+)\] \[(\w+)\] (\w+[\w\s]+) ([\w\/]+): (\w+[\w\s]+): (.*)"
 pattern2 = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) (\w+): (\w+): (\w+)"
 
 # Open the log file
@@ -41,6 +47,8 @@ with open(log_file, "r") as f:
                     "action": match2.group(4),
                 }
                 log_entries.append(log_entry)
+            else:
+                unmatched_entries.append(line)
 
 output_string = ""
 for entry in log_entries:
@@ -54,4 +62,11 @@ output_string = output_string[:-2]  # remove the trailing comma and newline
 with open(json_file, "w") as f:
     f.write(output_string)
 
-print("Log entries written to", json_file)
+# Open the unmatched log entries file
+with open(unmatched_file, "w") as f:
+    for line in unmatched_entries:
+        f.write(line)
+
+print("Matched Log entries written to", json_file)
+print("Unmatched Log entries written to", unmatched_file)
+
