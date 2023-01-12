04a285e5cf8c45b7d3b064112e28ee384e80030d
CLeoDayZero <cyril.leo@dayzerodiagnostics.com>
2023-01-12 13:41:21 -0500

    added more patterns and decerned between pattern 4 and other patterns

--- CDPGUI Debug Log Parser.py
+++ CDPGUI Debug Log Parser.py
@@ -20,35 +20,44 @@ unmatched_entries = []
 # Define the patterns for the different log formats
 pattern1 = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) \[(\d+)\] \[(\w+)\] (\w+[\w\s]+) ([\w\/]+): (\w+[\w\s]+): (.*)"
 pattern2 = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) (\w+): (\w+): (\w+)"
+pattern3 = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) \[(\d+)\] \[(\w+)\] (\w+[\w\s]+) ([\w\/]+): (.*)"
+pattern4 = r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) \[(\d+)\] \[(\w+)\] (\w+[\w\s_]+)"
+
+patterns = [pattern1, pattern2, pattern3, pattern4]
 
 # Open the log file
 with open(log_file, "r") as f:
     # Read each line of the file
     for line in f:
-        match1 = re.match(pattern1, line)
-        if match1:
+        match = None
+        for pattern in patterns:
+            match = re.match(pattern, line)
+            if match:
+                break
+        if match:
+            #print(pattern)
             log_entry = {
-                "date_time": match1.group(1),
-                "thread_id": match1.group(2),
-                "log_level": match1.group(3),
-                "subsystem": match1.group(4),
-                "component": match1.group(5),
-                "action": match1.group(6),
-                "guid": match1.group(7)
+                "date_time": match.group(1),
+                "thread_id": match.group(2),
+                "log_level": match.group(3),
+                #"subsystem": match.group(4),
+                # "pattern_used": pattern.__str__()
             }
+            if pattern == pattern4:
+                log_entry["message"] = match.group(4)
+            else:
+                log_entry["subsystem"] = match.group(4)
+            if len(match.groups()) == 5:
+                log_entry["component"] = match.group(5)
+                log_entry["action"] = match.group(6)
+            if len(match.groups()) == 7:
+                log_entry["component"] = match.group(5)
+                log_entry["action"] = match.group(6)
+                log_entry["guid"] = match.group(7)
             log_entries.append(log_entry)
         else:
-            match2 = re.match(pattern2, line)
-            if match2:
-                log_entry = {
-                    "date_time": match2.group(1),
-                    "subsystem": match2.group(2),
-                    "component": match2.group(3),
-                    "action": match2.group(4),
-                }
-                log_entries.append(log_entry)
-            else:
-                unmatched_entries.append(line)
+            unmatched_entries.append(line)
+
 
 output_string = ""
 for entry in log_entries:
